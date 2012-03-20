2ea469954a479aa407b133fc730dc2e493f69808
James Hollingworth <jamiehollingworth@gmail.com>
2012-03-20 09:32:48 +0000

    fixed ie bug

--- src/bootstrap-wysihtml5.js
+++ src/bootstrap-wysihtml5.js
@@ -38,6 +38,9 @@
 		this.editor =  new wysi.Editor(this.el.attr('id'), {
     		toolbar: this.toolbar.attr('id')
   		});
+  		if($.browser.msie) {
+  			this.el.hide();
+  		}
 	};
 
 	Wysihtml5.prototype = {
