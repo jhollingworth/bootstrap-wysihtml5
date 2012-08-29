2f038041cea6860abaae966b82d18c322032362f
Vincent ROBERT <vincent.robert@nanego.com>
2012-08-29 10:41:58 +0400

    French translation

--- src/locales/bootstrap-wysihtml5.fr-FR.js
+++ src/locales/bootstrap-wysihtml5.fr-FR.js
@@ -1,34 +1,34 @@
 /**
- * Brazilian portuguese translation for bootstrap-wysihtml5
+ * French translation for bootstrap-wysihtml5
  */
 (function($){
-    $.fn.wysihtml5.locale["pt-BR"] = {
+    $.fn.wysihtml5.locale["fr-FR"] = {
         font_styles: {
-              normal: "Texto normal",
-              h1: "Título 1",
-              h2: "Título 2"
+              normal: "Texte normal",
+              h1: "Titre 1",
+              h2: "Titre 2"
         },
         emphasis: {
-              bold: "Negrito",
-              italic: "Itálico",
-              underline: "Sublinhado"
+              bold: "Gras",
+              italic: "Italique",
+              underline: "Souligné"
         },
         lists: {
-              unordered: "Lista",
-              ordered: "Lista numerada",
-              outdent: "Outdent",
-              indent: "Indent"
+              unordered: "Liste à puces",
+              ordered: "Liste numérotée",
+              outdent: "Diminuer le retrait",
+              indent: "Augmenter le retrait"
         },
         link: {
-              insert: "Inserir link",
-              cancel: "Cancelar"
+              insert: "Insérer un lien",
+              cancel: "Annuler"
         },
         image: {
-              insert: "Inserir imagem",
-              cancel: "Cancelar"
+              insert: "Insérer une image",
+              cancel: "Annuler"
         },
         html: {
-            edit: "Editar HTML"
+            edit: "Editer en HTML"
         }
     };
 }(jQuery));
