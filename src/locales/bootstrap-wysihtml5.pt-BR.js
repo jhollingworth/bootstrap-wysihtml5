07a606595c6063d7e1839bd86968c2e34c4ca501
volmer <volmerius@gmail.com>
2012-09-20 19:09:04 -0300

    Updated translation for brazilian portuguese

--- src/locales/bootstrap-wysihtml5.pt-BR.js
+++ src/locales/bootstrap-wysihtml5.pt-BR.js
@@ -4,31 +4,45 @@
 (function($){
     $.fn.wysihtml5.locale["pt-BR"] = {
         font_styles: {
-              normal: "Texto normal",
-              h1: "Título 1",
-              h2: "Título 2"
+            normal: "Texto normal",
+            h1: "Título 1",
+            h2: "Título 2",
+            h3: "Título 3"
         },
         emphasis: {
-              bold: "Negrito",
-              italic: "Itálico",
-              underline: "Sublinhado"
+            bold: "Negrito",
+            italic: "Itálico",
+            underline: "Sublinhado"
         },
         lists: {
-              unordered: "Lista",
-              ordered: "Lista numerada",
-              outdent: "Outdent",
-              indent: "Indent"
+            unordered: "Lista",
+            ordered: "Lista numerada",
+            outdent: "Remover indentação",
+            indent: "Indentar"
         },
         link: {
-              insert: "Inserir link",
-              cancel: "Cancelar"
+            insert: "Inserir link",
+            cancel: "Cancelar"
         },
         image: {
-              insert: "Inserir imagem",
-              cancel: "Cancelar"
+            insert: "Inserir imagem",
+            cancel: "Cancelar"
         },
         html: {
             edit: "Editar HTML"
+        },
+        colours: {
+            black: "Preto",
+            silver: "Prata",
+            gray: "Cinza",
+            maroon: "Marrom",
+            red: "Vermelho",
+            purple: "Roxo",
+            green: "Verde",
+            olive: "Oliva",
+            navy: "Marinho",
+            blue: "Azul",
+            orange: "Laranja"
         }
     };
 }(jQuery));
