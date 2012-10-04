60bf2e88e0183eb647f70901b76eca0ad4142144
Alexandr Jeliuc <jeliucalexandr@gmail.com>
2012-10-04 16:44:29 +0300

    Russian locales file added

---
+++ src/locales/bootstrap-wysihtml5.ru-RU.js
@@ -0,0 +1,48 @@
+/**
+ * Rusian translation for bootstrap-wysihtml5
+ */
+(function($){
+    $.fn.wysihtml5.locale["ru-RU"] = {
+        font_styles: {
+              normal: "Нормальный текст",
+              h1: "Заголовок 1",
+              h2: "Заголовок 2"
+        },
+        emphasis: {
+              bold: "Полужирный",
+              italic: "Курсив",
+              underline: "Подчеркнутый"
+        },
+        lists: {
+              unordered: "Неупорядоченый",
+              ordered: "Упорядоченый",
+              outdent: "Выступ",
+              indent: "Отступ"
+        },
+        link: {
+              insert: "Вставить ссылку",
+              cancel: "Отмена"
+        },
+        image: {
+              insert: "Вставить изображение",
+              cancel: "Отмена"
+        },
+        html: {
+            edit: "Изменить HTML"
+        },
+
+        colours: {
+            black: "Черный",
+            silver: "Серебристый",
+            gray: "Серый",
+            maroon: "Каштановый",
+            red: "Красный",
+            purple: "Пурпурный",
+            green: "Зеленый",
+            olive: "Оливковый",
+            navy: "Морской",
+            blue: "Голубой",
+            orange: "Оранжевый"
+        }
+    };
+}(jQuery));
